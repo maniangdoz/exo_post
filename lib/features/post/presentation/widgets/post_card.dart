b007f210a0f5ae0467945f461087570c1b5dbf8b
WinnieParton <wkoumwonkoagni@myges.fr>
2023-10-22 15:17:57 +0200

    add comment

--- lib/features/post/presentation/widgets/post_card.dart
+++ lib/features/post/presentation/widgets/post_card.dart
@@ -84,7 +84,7 @@ class _PostCardState extends State<PostCard>
                 padding:
                     EdgeInsets.only(left: 18, right: 18, top: 10, bottom: 10),
                 child: ReadMoreText(
-                  "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page,",
+                  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
                   trimLines: 2,
                   colorClickableText: AppColors.accentColor,
                   trimMode: TrimMode.Line,
@@ -99,8 +99,18 @@ class _PostCardState extends State<PostCard>
                   ),
                 ),
               ),
-              const Row(
-                children: [],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.mode_comment),
+                  ),
+                  const Text(
+                    '12', // Replace with the actual number of comments
+                  ),
+                  const SizedBox(width: 10), // Adjust the width as needed
+                ],
               ),
             ],
           ),
